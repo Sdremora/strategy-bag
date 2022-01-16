1f43b699e5d3c6a5cbe7c85cf1dcb825d416332d
sdremora <sdremora@yandex-team.ru>
2022-01-16 21:34:15 +0300

    init

---
+++ src/main/java/ru/sinys/strategy/bag/StrategyBagApplication.java
@@ -0,0 +1,13 @@
+package ru.sinys.strategy.bag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrategyBagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StrategyBagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/sinys/strategy/bag/StrategyBagApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.sinys.strategy.bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrategyBagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
